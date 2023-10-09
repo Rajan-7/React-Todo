cd3c82afdc2a73159ef707e29beb07e0331064c1
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2023-10-09 23:00:52 +0545

    Component:Todo.jsx created,style.css create

---
+++ src/components/To-do/Todo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.css"
+
+const Todo = () => {
+  return (
+    <div>
+      INitial to do phase
+    </div>
+  )
+}
+
+export default Todo
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
+import Todo from './components/To-do/Todo';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Todo />
   </React.StrictMode>
 );
 

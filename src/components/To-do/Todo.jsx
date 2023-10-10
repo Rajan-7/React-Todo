44ee5266ac2d4c6c7a4de98a8be230a0e61ea320
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2023-10-10 23:22:47 +0545

    Modification:Todo.jsx

--- src/components/To-do/Todo.jsx
+++ src/components/To-do/Todo.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import "./style.css"
+import React from "react";
+import "./style.css";
 
 const Todo = () => {
   return (
-    <div>
-      INitial to do phase
-    </div>
-  )
-}
+    <>
+      <div className="main-container">
+        <div className="sub-container">
+          <figure>
+            <img src="./images/Todo.svg" alt="todologo" />
+            <figcaption>Add Your List Here ✌️</figcaption>
+          </figure>
+          <div className="addItems">
+            <input
+              type="text"
+              placeholder="🖍️ Add Items"
+              className="input-class"
+            />
+          </div>
+          <div className="showItems">
+            <div className="eachItem">
+              <h3>Coding in react</h3>
+              <i className="far fa-edit add-btn"></i>
+              <i className="far fa-trash-alt add-btn"></i>
+            </div>
+          </div>
+          <div className="showItems">
+            <button><span>CHECK LIST</span></button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Todo
+export default Todo;
